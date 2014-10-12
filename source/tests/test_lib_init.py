32ca28b9ef979e16df7ff26623b495e3aa942e5f
wequbelity <wequbelity@gmail.com>
2014-10-12 17:07:47 +0400

    added some lib_init_test 17 BrMiss

--- source/tests/test_lib_init.py
+++ source/tests/test_lib_init.py
@@ -58,6 +58,212 @@ class GetCountersTestCase(unittest.TestCase):
 
 
 class CheckForMetaTestCase(unittest.TestCase):
+    @mock.patch('source.lib.__init__.BeautifulSoup')
+    def test_not_soup_find(self, BeautifulSoup):
+        content = mock.MagicMock()
+        url = '1'
+
+        soup = mock.MagicMock()
+        soup.find.return_value = False
+        BeautifulSoup.return_value = soup
+
+        source.lib.__init__.check_for_meta(content, url)
+        soup.find.assert_called_once_with("meta")
+
+    @mock.patch('source.lib.__init__.re.search')
+    @mock.patch('source.lib.__init__.urljoin')
+    @mock.patch('source.lib.__init__.BeautifulSoup')
+    def test_soup_find(self, BeautifulSoup, urljoin, re_search):
+        content = mock.MagicMock()
+        url = '42'
+
+        attr = mock.MagicMock()
+        content_mock = mock.MagicMock()
+
+        soup = mock.MagicMock()
+        result = mock.MagicMock()
+        wait_mock = mock.MagicMock()
+        text_mock = mock.MagicMock()
+        text_mock.strip.return_value = text_mock
+        result.__getitem__('content').split.return_value = (wait_mock, text_mock)
+
+        result_mock = mock.MagicMock()
+        re_search.return_value = result_mock
+        result_mock.return_value = ['42']
+
+        return_assert = '42'
+        urljoin.return_value = '42'
+
+        result.attrs = {
+            'content' : 'content',
+            'http-equiv' : 'refresh'
+        }
+
+        soup.find.return_value = result
+        BeautifulSoup.return_value = soup
+
+
+        assert  return_assert == source.lib.__init__.check_for_meta(content, url)
+        soup.find.assert_called_once_with("meta")
+        result.__getitem__('content').split.assert_called_once_with(";")
+
+    @mock.patch('source.lib.__init__.re.search')
+    @mock.patch('source.lib.__init__.BeautifulSoup')
+    def test_soup_find_re_not_found(self, BeautifulSoup, re_search):
+        content = mock.MagicMock()
+        url = '42'
+
+        attr = mock.MagicMock()
+        content_mock = mock.MagicMock()
+
+        soup = mock.MagicMock()
+        result = mock.MagicMock()
+        wait_mock = mock.MagicMock()
+        text_mock = mock.MagicMock()
+        text_mock.strip.return_value = text_mock
+        result.__getitem__('content').split.return_value = (wait_mock, text_mock)
+
+        re_search.return_value = False
+
+        result.attrs = {
+            'content' : 'content',
+            'http-equiv' : 'refresh'
+        }
+
+        soup.find.return_value = result
+        BeautifulSoup.return_value = soup
+
+
+        source.lib.__init__.check_for_meta(content, url)
+        soup.find.assert_called_once_with("meta")
+        result.__getitem__('content').split.assert_called_once_with(";")
+
+
+    @mock.patch('source.lib.__init__.BeautifulSoup')
+    def test_soup_find_len_less_2(self, BeautifulSoup):
+        content = mock.MagicMock()
+        url = '42'
+
+        attr = mock.MagicMock()
+        content_mock = mock.MagicMock()
+
+        soup = mock.MagicMock()
+        result = mock.MagicMock()
+        result.__getitem__('content').split.return_value = "1"
+
+        result.attrs = {
+            'content' : 'content',
+            'http-equiv' : 'refresh'
+        }
+
+        soup.find.return_value = result
+        BeautifulSoup.return_value = soup
+
+        source.lib.__init__.check_for_meta(content, url)
+        soup.find.assert_called_once_with("meta")
+        result.__getitem__('content').split.assert_called_once_with(";")
+
+    @mock.patch('source.lib.__init__.BeautifulSoup')
+    def test_soup_find_attr_not_found(self, BeautifulSoup):
+        content = mock.MagicMock()
+        url = '42'
+
+        attr = mock.MagicMock()
+        content_mock = mock.MagicMock()
+
+        soup = mock.MagicMock()
+        result = mock.MagicMock()
+        result.__getitem__('content').split.return_value = "1"
+
+        result.attrs = {
+            'content' : 'content'
+        }
+
+        soup.find.return_value = result
+        BeautifulSoup.return_value = soup
+
+        source.lib.__init__.check_for_meta(content, url)
+        soup.find.assert_called_once_with("meta")
+        assert 0 == result.__getitem__('content').call_count
 
     pass
 # check_for_meta(content, url) tests end
+
+
+# fix_market_url(url) tests 
+
+
+class FixMarketUrlTestCase(unittest.TestCase):
+    def test_main(self):
+        url = mock.MagicMock()
+        url.lstrip.return_value = 'yeah'
+
+        result_url = 'http://play.google.com/store/apps/' + 'yeah'
+
+        assert result_url == source.lib.__init__.fix_market_url(url)
+        url.lstrip.assert_called_once_with("market://"); 
+
+    pass 
+# fix_market_url(url) tests end
+
+
+# make_pycurl_request(url, timeout, useragent=None) tests
+
+class MakePycurlRequestTestCase(unittest.TestCase):
+    
+    @mock.patch('source.lib.__init__.pycurl')
+    @mock.patch('source.lib.__init__.StringIO')
+    @mock.patch('source.lib.__init__.to_str')
+    @mock.patch('source.lib.__init__.to_unicode')
+    def test_all_branch(self, to_unicode, to_str, StringIO, pycurl):
+        url = mock.MagicMock()
+        timeout = 1
+        useragent = mock.Mock()
+
+        curl = mock.MagicMock()
+        pycurl.Curl.return_value = curl
+
+        redirect_url = mock.MagicMock() 
+        curl.getinfo.return_value = redirect_url
+        curl.USERAGENT = '1'
+
+        to_unicode.return_value = '24'
+
+        buff = mock.MagicMock()
+        buff.getvalue.return_value = '42'
+        StringIO.return_value = buff
+
+        assert ('42', '24') == source.lib.__init__.make_pycurl_request(url, timeout, useragent)
+        curl.setopt.assert_any_call('1', useragent)
+        curl.close.assert_called_once_with()
+
+    @mock.patch('source.lib.__init__.pycurl')
+    @mock.patch('source.lib.__init__.StringIO')
+    @mock.patch('source.lib.__init__.to_str')
+    def test_not_if_not_if(self, to_str, StringIO, pycurl):
+        url = mock.MagicMock()
+        timeout = 1
+
+        curl = mock.MagicMock()
+        pycurl.Curl.return_value = curl
+
+        redirect_url = None 
+        curl.getinfo.return_value = redirect_url
+
+        buff = mock.MagicMock()
+        buff.getvalue.return_value = '42'
+        StringIO.return_value = buff
+
+        assert ('42', redirect_url) == source.lib.__init__.make_pycurl_request(url, timeout)
+        curl.close.assert_called_once_with()
+
+
+# make_pycurl_request(url, timeout, useragent=None) tests end
+
+
+# prepare_url(url) tests
+
+class PrepareUrlTestCase(unittest.TestCase):
+    pass
+    # @patch('source.lib.__init__.')
+    # def test_all_branch(self):
